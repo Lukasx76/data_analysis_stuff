ed1695daea563c9fff529ccb3873f238e5ac016c
Lucas Feitoza Gomes <72843674+Lukasx76@users.noreply.github.com>
2023-01-28 18:23:37 -0300

    Update dash101.py

--- dash101.py
+++ dash101.py
@@ -1,13 +1,21 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
+
 
 app = dash.Dash()
 
 app.layout = html.Div([
     html.H1('hello world'),
-    html.Div('a new kind of application')
+    html.Div('a new kind of application'),
+    dcc.Graph(
+        figure={
+            'data' : [{'x' : [1,2,3,4,5,6], 'y' : [2,4,6,8,10,12], 'type': 'bar', 'name': 'graph1'},
+                      {'x' : [1,2,3,4,5,6], 'y' : [12,10,12,8,10,12], 'type': 'bar', 'name': 'graph2'}],
+            'layout' : {'title' : 'graph2'}
+        }
+    )
 ])
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
