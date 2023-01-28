4210888c502ad06dcf79363c9a517be2e841caf6
Lucas Feitoza Gomes <72843674+Lukasx76@users.noreply.github.com>
2023-01-28 18:37:21 -0300

    Update dash101.py
    Using a bit of css to change the colors of the background

--- dash101.py
+++ dash101.py
@@ -6,13 +6,23 @@ from dash import html
 app = dash.Dash()
 
 app.layout = html.Div([
-    html.H1('hello world'),
-    html.Div('a new kind of application'),
+    html.H1(children = 'Dash',
+    style = {
+        'textAlign' : 'center',
+        'color' : '#AA4A44'
+    }),
+    html.Div(children = 'A new approach of displaying plots',
+    style = {
+        'textAlign': 'center',
+        'color': '#AA4A44'
+    }),
     dcc.Graph(
         figure={
             'data' : [{'x' : [1,2,3,4,5,6], 'y' : [2,4,6,8,10,12], 'type': 'bar', 'name': 'graph1'},
                       {'x' : [1,2,3,4,5,6], 'y' : [12,10,12,8,10,12], 'type': 'bar', 'name': 'graph2'}],
-            'layout' : {'title' : 'graph2'}
+            'layout' : {'title' : 'graph2',
+            'plot_bgcolor' : '#19B8BD',
+            'paper_bgcolor' : '#19B8BD'}
         }
     )
 ])
